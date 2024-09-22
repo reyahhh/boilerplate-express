89be3f305ffc78efdb2350652dd18f85dc430415
reyahhh h <reyiahornejas+1@gmail.com>
2024-09-22 04:25:08 +0000

    update path

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 
-app.use("/public", express.static(__dirname + "/public/style.css"));
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
