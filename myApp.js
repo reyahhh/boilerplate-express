8e330e8d31de160aa85a3a8cd815bdcf0c5819b9
reyahhh h <reyiahornejas+1@gmail.com>
2024-10-05 15:34:37 +0000

    a

--- myApp.js
+++ myApp.js
@@ -1,18 +1,11 @@
 require('dotenv').config()
+var bodyParser = require("body-parser");
+
 let express = require('express');
 let app = express();
 
-app.get("/name", function(req, res) {
-  var firstName = req.query.first;
-  var lastName = req.query.last;
-  // OR you can destructure and rename the keys
-  var { first: firstName, last: lastName } = req.query;
-  // Use template literals to form a formatted string
-  res.json({
-    name: `${firstName} ${lastName}`
-  });
-});
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 
