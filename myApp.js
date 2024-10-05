a3294b55942e8b634979af9ffa8c8d2578ccda6f
reyahhh h <reyiahornejas+1@gmail.com>
2024-10-05 03:38:30 +0000

    update

--- myApp.js
+++ myApp.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 let express = require('express');
 let app = express();
 
-app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
-});
+// app.use("/public", express.static(__dirname + "/public"));
+// app.get("/", (req, res) => {
+//     res.sendFile(__dirname + "/views/index.html");
+// });
 
 app.get(
     "/now",
