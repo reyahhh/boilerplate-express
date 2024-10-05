2723c205be7308ad92ebe0bbf794e7227c232095
reyahhh h <reyiahornejas+1@gmail.com>
2024-10-05 15:10:50 +0000

    get

--- myApp.js
+++ myApp.js
@@ -2,16 +2,16 @@ require('dotenv').config()
 let express = require('express');
 let app = express();
 
-app.get(
-    "/:word/echo",
-    (req, res, next) => {
-      const {word} = req.params
-      res.json({
-        echo: word
-      })
-      next();
-    });
-
+app.get("/name", function(req, res) {
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  // OR you can destructure and rename the keys
+  var { first: firstName, last: lastName } = req.query;
+  // Use template literals to form a formatted string
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
+});
 
 
 
