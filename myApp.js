fb934b7d01c24c4aa71664b0d7bbb4058bd367d0
reyahhh h <reyiahornejas+1@gmail.com>
2024-10-05 03:43:55 +0000

    clean

--- myApp.js
+++ myApp.js
@@ -2,28 +2,16 @@ require('dotenv').config()
 let express = require('express');
 let app = express();
 
-// app.use("/public", express.static(__dirname + "/public"));
-// app.get("/", (req, res) => {
-//     res.sendFile(__dirname + "/views/index.html");
-// });
-
-app.get(
-    "/now",
-    (req, res, next) => {
-      // adding a new property to req object
-      // in the middleware function
-      req.time = new Date().toString();
-      res.send(req.time);
-      next();
-    },
-    (req, res) => {
-      // accessing the newly added property
-      // in the main function
-    //   res.json({"time": req.time});
-      res.send({"time": req.time});
-    }
-  );
-
+const middleware = (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  };
+  
+  app.get("/now", middleware, (req, res) => {
+    res.send({
+      time: req.time
+    });
+  });
 
 
 
