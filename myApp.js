422ce97eb27623779181cae11c15552e3a31e39c
reyahhh h <reyiahornejas+1@gmail.com>
2024-10-05 15:38:13 +0000

    adc

--- myApp.js
+++ myApp.js
@@ -4,11 +4,24 @@ var bodyParser = require("body-parser");
 let express = require('express');
 let app = express();
 
+app.get("/name", function(req, res) {
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  // OR you can destructure and rename the keys
+  var { first: firstName, last: lastName } = req.query;
+  // Use template literals to form a formatted string
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
+});
+
 app.post("/name", function(req, res) {
   // Handle the data in the request
   var string = req.body.first + " " + req.body.last;
   res.json({ name: string });
 });
+
+
 
 
 
